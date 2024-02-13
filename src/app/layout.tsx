16076b005a405d1a36c34b57dcf924fbac33d23c
Crisley Venâncio Marques <crisley.marques@ccc.ufcg.edu.br>
2024-02-13 20:56:52 -0300

    [chore] add fonts and css configurations

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,5 @@
 import type { Metadata } from "next";
-import { Inter, Rajdhani } from "next/font/google";
 import '../styles/global.css';
-
-const inter = Inter({ subsets: ["latin"] });
-const rajdhani = Rajdhani({ subsets: ["latin"], weight: ['600'] });
 
 export const metadata: Metadata = {
   title: "Move It",
@@ -17,7 +13,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
